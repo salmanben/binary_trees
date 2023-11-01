0ccb5716abae34edcb88bcb9af68867ec06d2485
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-11-01 09:19:17 +0100

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,11 +7,8 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0;
+	if (node == NULL || node->parent != NULL)
+		return (0);
 
-    if (node->parent == NULL)
-        return (1);
-
-    return (0);
+	return (1);
 }

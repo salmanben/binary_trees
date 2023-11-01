7fb925c35f676bd8d7ffe38ab79406d68d8acef4
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-11-01 09:44:42 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,14 +7,17 @@
  * 
  * Return: the number of leaves
  */
-size_t binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t left = 0, right = 0;
-
-		left = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		right = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		size_t left;
+                size_t right;
+              
+                left = 0;
+                right = 0;
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((left > right) ? left : right);
 	}
 	return (0);

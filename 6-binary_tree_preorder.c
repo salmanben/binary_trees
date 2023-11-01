edfc6b8610cb520b256d5521a9402314c35604b3
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-11-01 09:22:04 +0100

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,6 +4,7 @@
  * binary_tree_preorder - Traverse a binary tree using pre-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)

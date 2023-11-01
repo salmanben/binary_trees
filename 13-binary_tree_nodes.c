9afa6dd1442e47031ce1f218aa703d910f1cd681
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-11-01 09:46:19 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,7 +4,7 @@
  * binary_tree_nodes - counts counts the nodes
  * with at least 1 child in a binary tree
  * @tree: Pointer to the root
- * 
+ *
  * Return: the number of leaves
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)

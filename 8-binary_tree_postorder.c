955bc18dc39729826b98ea33d8fc3a3bf2cc8afb
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-11-01 09:23:53 +0100

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,12 +5,13 @@
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
-    binary_tree_preorder(tree->left, func);
-    func(tree->n);
-    binary_tree_preorder(tree->right, func);
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
 }
 

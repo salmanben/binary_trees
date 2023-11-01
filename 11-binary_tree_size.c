fafa6de67edc068cb1d9f568f4bed1184d79dcfc
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-11-01 09:31:27 +0100

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,7 +8,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (binary_tree_size(treee->left) + binary_tree_size(tree->right) + 1);
+	size_t tree_size = 0;
+
+	if (tree)
+	{
+		tree_size += 1;
+		tree_size += binary_tree_size(tree->left);
+		tree_size += binary_tree_size(tree->right);
+	}
+	return (tree_size);
 }
